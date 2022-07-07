f7a32fd62c679ee270eb90b06e56d1064109e150
Aman Bahukhandi <104689493+oldmonk018@users.noreply.github.com>
2022-07-07 21:23:01 +0530

    Add files via upload

---
+++ ReminderApp.py
@@ -0,0 +1,13 @@
+import time
+from plyer import notification
+from pathlib import Path
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+            title = "Please Drink Water",
+            message = """About 15.5 cups (3.7 liters) of fluids a day for men.
+            About 11.5 cups (2.7 liters) of fluids a day for women.""",
+            app_icon = "C:\Users\wwwam\Documents\rough-code\icon.ico",
+            timeout = 2
+        )
+        time.sleep(6)
